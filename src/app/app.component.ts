c86119984981184c529c403ed5b124e901a563b6
Andrey Andrusenko <aandrusenko@yandex.ru>
2024-09-26 13:00:05 +0300

    Refactoring and github link, assets

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,10 @@
 import { Component, ElementRef, HostListener, QueryList, ViewChildren} from '@angular/core';
 import { DeviceDetectorService } from 'ngx-device-detector';
-declare var VANTA:any;
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-
 export class AppComponent 
 {
   title = 'Andrew Andrusenko';
@@ -16,6 +13,7 @@ export class AppComponent
   @ViewChildren('reveal') sectionsToReveal :QueryList<ElementRef>
   @HostListener('document:scroll', ['$event'])
   onScrollReveal(event: Event) {  
+    
      this.sectionsToReveal.forEach(el=>{
       let elTop = el.nativeElement.getBoundingClientRect().top
       let windowTop = window.innerHeight
@@ -44,17 +42,4 @@ export class AppComponent
       break;
     }
   }
-  ngOnInit(): void {
-/*     VANTA.TOPOLOGY({
-      el: "#animated-background",
-      mouseControls: false,
-      touchControls: false,
-      gyroControls: false,
-      minHeight: 200.00,
-      minWidth: 200.00,
-      scale: 1.00,
-      scaleMobile: 1.00
-    }) */
-  }
-
 }
